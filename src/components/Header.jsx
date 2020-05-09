42d29ff620c6a2ade8bb6a7efd2873ef67e46ccc
Zach Johnson <zlg23johnson@gmail.com>
2020-05-08 20:50:40 -0500

    Added contact to header, broke a bit of styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,15 @@ import React from "react";
 import GithubLogo from '../assets/images/github.svg';
 
 function Header() {
-    return (
-      <header className="App-header">
-        <h1>Ballcapz</h1>
-        <div className="social-links">
-          <a href="https://github.com/Ballcapz"><img src={GithubLogo} alt="Github"/></a>
-        </div>
-      </header>
-    );
+  return (
+    <header className="App-header">
+      <h1>Ballcapz</h1>
+      <div className="social-links">
+        <a href="https://github.com/Ballcapz"><img src={GithubLogo} alt="Github" /></a>
+        <a href="mailto:zachary.a.johnson.10@gmail.com">Contact</a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
