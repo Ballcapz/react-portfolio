f6a0a8d72ce451dacc54bcc929a1afd37eeeb95f
Zach Johnson <zlg23johnson@gmail.com>
2020-05-08 20:19:17 -0500

    Added in 2 nav buttons for the future, and a projects section for the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 // components
 import Header from "./components/Header";
+import Links from "./components/Links";
 
 function App() {
   return (
     <div className={"App"}>
       <Header />
+      <Links />
+      <h2 className="projects">Projects</h2>
+      <Projects />
+    </div>
+  );
+}
+
+function Projects() {
+
+  const [projects, setProjects] = useState([]);
+
+  const projectsUrl = `http://localhost:3000/projects`;
+  const getProjects = () => {
+    fetch(projectsUrl)
+      .then(res => res.json())
+      .then(response => {
+        setProjects(response)
+      })
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    getProjects()
+  }, []);
+
+  return (
+    <div>
+      {projects.map(function (proj, index) {
+        return <Project key={index} project={proj} />;
+      })}
+    </div>
+  );
+}
+
+
+function Project(props) {
+  return (
+    <div>
+      <h3>{props.project.title}</h3>
+      <a href={props.project.url}>{props.project.url}</a>
     </div>
   );
 }
---
+++ src/components/Links.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Links.css';
+
+function Links() {
+    return (
+        <section className="Nav-Buttons">
+            <a href="/blog">Blog</a>
+            <a href="/about">About</a>
+        </section>
+    );
+}
+
+export default Links;
